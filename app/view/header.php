ec3e4a3ea0fab26d52817f0a9727121b76cc0bea
程宇枫 <chengyufeng@Cyf-MBP.local>
2017-07-30 16:08:03 +0800

    1

---
+++ app/view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Blog</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u">
+</head>
+<body>
+	<script type="text/javascript" src="/assets/js/flexible.js"></script>
+	<script type="text/javascript" src="/assets/js/flexible_css.js"></script>
+	<script type="text/javascript" src="/assets/js/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="/assets/js/react.js"></script>
+	<script type="text/javascript" src="/assets/js/react-dom.js"></script>
+	<script type="text/javascript" src="/assets/js/borwer.min.js"></script>
+	<script type="text/javascript" src="/assets/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
